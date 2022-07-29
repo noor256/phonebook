b245d4a647ed3003a2113e8dbab12d86efe88d0a
ibrahima <lodiajames@hotmail.com>
2022-07-29 15:42:37 -0400

    db integration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Filter from "./components/Filter";
 import Button from "./components/Button";
 import Name from "./components/name";
 import Number from "./components/Number";
-const App=()=>{
-  const [persons, setPersons] = useState([    
-  { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }])
+import axios from 'axios';
 
+
+
+
+const App=()=>{
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('Arto Hella')
   const [newPhone, setNewPhone] = useState([])
   const [searchName, setSearchName] = useState("")
 
 
   const placeholder = 'Search for names'
+  useEffect(() => {
+    console.log('effect')
 
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+  }, [])
   const addPerson=(event)=>{
     event.preventDefault()
 
